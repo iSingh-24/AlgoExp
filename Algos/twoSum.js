5cdca405ed655f6c01fb8f508839eb6c4faf7a28
Inderprit Singh <singhinderprit1992@gmail.com>
2025-05-03 17:34:12 -0400

    first code commit

---
+++ Algos/twoSum.js
@@ -0,0 +1,5 @@
+/**
+ * Given a target value and an array of integers, write an algorithm that returns the indices of two integers that add up to the target num
+ */
+
+const twoSum = (nums, target) => {};
